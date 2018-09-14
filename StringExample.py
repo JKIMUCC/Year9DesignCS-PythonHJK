d42f9ca4af92386d3f9891410457d6190f4f6685
Jace Kim <jace.kim@0102.local>
2018-09-14 09:45:14 -0400

    NEW

---
+++ StringExample.py
@@ -0,0 +1,26 @@
+#THis file will go through the basics of
+#String Manipulation
+
+#Strings are collections of characters
+#String are enclosed in " " or ' '
+# "Paul"
+# "Paul is cool"
+# "Paul is cool!"
+
+#Two things we need to talk about
+#when we think of strings
+#index - Always starts at 0
+#Length
+
+#Example
+# 0123		012345
+# "Paul"		"Monkey"
+# len("Paul") = 4
+# len("Monkey") = 6
+
+name = "Jace"
+
+print(name) #I can print strings
+
+sentence = name + "is cool"
+print(sentence)
