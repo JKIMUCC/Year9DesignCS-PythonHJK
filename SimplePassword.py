8b3f165cc58bc9611ffaf4c70b5ea2cb90d8c00a
Jace Kim <jace.kim@0102.local>
2018-09-18 14:58:11 -0400

    New

---
+++ SimplePassword.py
@@ -0,0 +1,7 @@
+username = input("Username: ")
+password = input("Password: ")
+if username == "Jace Kim":
+	if password == 'asdfas':
+		print("Password accepted")
+	else: 
+		print("LEAVE THIS COMPUTER ALONE")
