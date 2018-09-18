c28a08b1934a77924cead9f7ff19c8e24a1e2ec0
Jace Kim <jace.kim@0102.local>
2018-09-18 15:16:47 -0400

    New testing

--- SimplePassword.py
+++ SimplePassword.py
@@ -1,7 +1,13 @@
 username = input("Username: ")
 password = input("Password: ")
 if username == "Jace Kim":
-	if password == 'asdfas':
+	if password == 'Heyyyy':
 		print("Password accepted")
+	else: 
+		print("LEAVE THIS COMPUTER ALONE")
+
+if username == "Jong":
+	if password == ' ':
+		print("Welcome toxic Garen Main")
 	else: 
 		print("LEAVE THIS COMPUTER ALONE")
