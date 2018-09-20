92618fe74a1a969836a5cc126caa64016f93b0ba
Jace Kim <jace.kim@0102.local>
2018-09-20 00:09:10 -0400

    Update

--- SimplePassword.py
+++ SimplePassword.py
@@ -1,15 +1,21 @@
 # Made By Jace Kim 9/18/18
 #Modified by 
+Import sys
+
 username = input("Username: ")
 password = input("Password: ")
-if username == "Jace Kim":
-	if password == 'Heyyyy':
+if username == "Username":
+	if password == 'Password':
 		print("Password accepted")
 	else: 
 		print("LEAVE THIS COMPUTER ALONE")
+		sys.exit()
 
+
+#Ignore bottom code
 if username == "Jong":
 	if password == ' ':
 		print("Welcome toxic Garen Main")
 	else: 
-		print("LEAVE THIS COMPUTER ALONE")
+		print("BYE BYE")
+		sys.exit()
