032a72d91da16645e9b1347266b9a5b5a7aa98b3
Jace Kim <jace.kim@0102.local>
2018-10-02 12:10:29 -0400

    NEW

---
+++ LoopDemoFor.py
@@ -0,0 +1,17 @@
+print("*********")
+print("Counted Loops: For")
+
+print("0")
+print("1")
+print("2")
+print("3")
+print("4")
+
+print("***************************")
+
+for i in range(-44,137,1):
+	print(i)
+	if(i % 2 == 0):
+		print(str(i)+" is even")
+	else:
+		print(str(i)+" is odd")
