f472c323b09c9504ad144cd8d1e5600ae45dbbb7
Jace Kim <jace.kim@0102.local>
2018-12-14 21:15:29 -0500

    update

--- PaceCalculatorPage.py
+++ PaceCalculatorPage.py
@@ -1,86 +1,85 @@
 import tkinter as tk
 from tkinter import *
+import math
 
 
 
 
-root = tk.Tk()
-root.configure(background = "navy")
-root.geometry('400x400')
 
-def clear_search(event):
-   entTime.delete(0, END) 
-def clear_search2(event):
-   entDS.delete(0, END) 
 
 class paceCalculator:
-    def __init__(self):
-        window = Tk()
-        window.title("Pace Calculator")
 
-        def result(z1,z2,z3):
-            biz=(z1/z2)*z3
-            lblresult.delete(0,END)
-            lblresult.insert(0,biz)
-            return
+	#def clear_search(event):
+		#self.entTime.delete(0, END)
+	#def clear_search2(event):
+		#self.entDS.delete(0, END)
 
-        labTitle = tk.Label(root,text = "Pace Calculator")
-        labTitle.grid(row = 2, column = 0, columnspan = 2)
-        labTime = tk.Label(root, text = "Enter Time: ")
-        labTime.grid(row = 3, column = 0)
+	def __init__(self):
+		self.root = tk.Tk()
+		self.root.configure(background = "navy")
+		self.root.geometry('400x400')
 
-        self.num1Var = IntVar()
-        entTime = tk.Entry(root, background = "light grey", self.num1Var, justify = RIGHT)
-        entTime.grid(row = 3, column = 1)
-        entTime.insert(0,"Enter Time in Seconds")
-        entTime.bind("<Button-1>", clear_search)
-        labDS = tk.Label(root, text = "Enter Distance Swam: ")
-        labDS.grid(row = 4, column = 0, padx = 10, pady = 10)
+		self.labTitle = tk.Label(self.root,text = "Pace Calculator")
+		self.labTitle.grid(row = 2, column = 0, columnspan = 2)
 
-        self.num2Var = IntVar()
-        entDS = tk.Entry(root, background = "light grey", self.num2Var, justify = RIGHT)
-        entDS.grid(row = 4, column = 1, padx = 10, pady = 10)
-        entDS.insert(0,"Enter Distance in Meters")
-        entDS.bind("<Button-1>", clear_search2) 
+		self.labTime = tk.Label(self.root, text = "Enter Time in Seconds: ")
+		self.labTime.grid(row = 3, column = 0)
+		self.entTime = tk.Entry(self.root, background = "light grey")
+		self.entTime.grid(row = 3, column = 1)
+		#self.entTime.insert(0,"Enter Time in Seconds")
+		#self.entTime.bind("<Button-1>", clear_search)
 
-        DDLabel = Label(text = "Select Distance: ")
-        DDLabel.grid(row = 5 , column = 0, sticky = "E")
+		self.labDS = tk.Label(self.root, text = "Enter Distance Swam: ")
+		self.labDS.grid(row = 4, column = 0, padx = 10, pady = 10)
+		self.entDS = tk.Entry(self.root, background = "light grey")
+		self.entDS.grid(row = 4, column = 1, padx = 10, pady = 10)
+		#self.entDS.insert(0,"Enter Distance in Meters")
+		#self.entDS.bind("<Button-1>", clear_search2) 
 
-        OPTIONS = [ 
-        "25",
-        "50",
-        "100",
-        "200",
-        ]
+		self.DDLabel = Label(self.root, text = "Select Distance: ")
+		self.DDLabel.grid(row = 5 , column = 0, sticky = "E")
 
-        var = tk.StringVar(root)
-        var.set(OPTIONS[0])
+		OPTIONS = [ 
+		"25",
+		"50",
+		"100",
+		"200",
+		]
 
-        self.num3Var = IntVar()
-        dropDownMenu = tk.OptionMenu(root,var, OPTIONS[0], OPTIONS[1], OPTIONS[2], OPTIONS[3], self.num3Var, justify = RIGHT)
-        dropDownMenu.grid(row = 5 , column = 1, sticky = "W")
+		self.var = tk.StringVar(self.root)
+		self.var.set(OPTIONS[0])
 
-        btnCalc = tk.Button(root, text = "Calculate", command ,command=lambda:result(self.num1Var.get(),self.num2Var.get(), self.num3Var.get())
-        btnCalc.grid(row = 6 , column = 0, columnspan = 2)
+		self.dropDownMenu = tk.OptionMenu(self.root,self.var, OPTIONS[0], OPTIONS[1], OPTIONS[2], OPTIONS[3])
+		self.dropDownMenu.grid(row = 5 , column = 1, sticky = "W")
 
-        output = tk.Entry(root, background = "light grey", height = 10, width = 50, font = ("Arial",12))
-        output.config(state = "disable")
-        output.grid(row = 7 ,column = 0, columnspan = 2)
+		self.btnCalc = tk.Button(self.root, text = "Calculate",command= self.calculate)
+		self.btnCalc.grid(row = 6 , column = 0, columnspan = 2)
+		self.output = tk.Text(self.root, height = 10, width=50, relief=tk.GROOVE)
+		self.output.config(state="disabled")
+		self.output.grid(row = 8, column = 0, columnspan = 2)
 
+		self.root.mainloop()
 
 
+	def calculate(self):
+		print("Calculating")
+
+		t = float(self.entTime.get())
+		d = float(self.entDS.get())
+		c = float(self.var.get())
+
+		r = math.ceil((t/d)*c)
 
 
+		outputValue = "When "+str(t)+" seconds is taken to swim "+str(d)+" meters,   the pace per "+str(c)+"meters is:" + str(r) + " seconds per " + str(c)
 
 
+		self.output.config(state = "normal")
 
-
-
-
-
-
-
-		root.mainloop()
+		self.output.delete(1.0,tk.END)
+		self.output.insert(tk.INSERT,outputValue)
+		self.output.config(state="disabled")
+		print(str(r) + "Seconds per" + str(c))
 
 paceCalculator()
 
